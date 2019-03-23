0f80ba46626a5e6599fcb588eff9dfb94b543f83
Bonnie <bonnieacuna@gmail.com>
2019-03-23 14:00:00 -0700

    first draft

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./pages/Navbar";
+import Features from "./pages/Features";
+import Body from "./pages/Body";
+
+class App extends Component {
+render() {
+    return (
+      <div>
+        <Navbar />
+        
+        <Features />
+        <Body>
+          
+        </Body>
+        <Footer />
+      </div>
+    );
+  };
+}
+
+export default App;
---
+++ client/src/components/Features.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Features = (props) =>
+      <div className="container text-center image-grid">
+        {props.children}
+      </div>
+
+export default Features;
