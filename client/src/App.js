db6594b4cac55752486867bf2934d220848281af
AngelMD55 <angel_demerutis@hotmail.com>
2019-03-25 21:24:40 -0700

    added Router to App.js and created pares folder with signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Search from "../src/components/Search/index.js";
+import Signup from "./pages/Signup/Signup"
 
 class App extends Component {
-render() {
+  render() {
     return (
-      <div>
-        <Navbar />
-        <Search />
-        {/* <Footer /> */}
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Search />
+          <Switch>
+            <Route exact path = "/signup" component={Signup} />
+          </Switch>
+           {/* <Footer /> */}
+        </div>
+      </Router>
     );
   };
 }
