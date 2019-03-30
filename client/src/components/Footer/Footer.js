5cab91962f81992a87c3d6ed1ce9480cd156c534
Scripted Well <whigareda@theblackpilledpage.com>
2019-03-29 18:01:15 -0700

    More Foot Work

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import { Nav, NavItem } from 'react-bootstrap';
+import { Container, Row } from '../Grid/index';
 
-function Footer(/*props*/) {
-  return (
-    <footer>
-      <div>
-        <Nav justified>
-          <NavItem
-            eventKey={1}>
-            About Us
-          </NavItem>
-
-          <NavItem
-            eventKey={2}>
-            Contact Us
-          </NavItem>
-        </Nav>
-
-        <div className="text-center small copyright">
-          © Clean Code Crew 2019
-        </div>
-      </div>
-    </footer>
-  );
+class Footer extends React.Component {
+    render(){
+        return(
+            <footer className="footer text-center">
+                <div className="container">
+                    <p className="footer-text">Copyright 2017 Clean Code Crew. All Rights Reserved</p>
+                </div>
+            </footer>
+        );
+    } 
 }
-
-export default Footer;
+export default Footer
