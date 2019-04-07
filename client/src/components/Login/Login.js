37c27bf3698c0abf0f14619e7ecb719e30978ebd
Scripted Well <whigareda@theblackpilledpage.com>
2019-04-07 16:24:41 -0700

    styling changes to sign-up/login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Input } from "../Form/index"
-import { Container, Row } from "../../components/Grid/index";
+import React from "react";
+//import { Input } from "../Form/index";
+//import { Container, Row } from "../../components/Grid/index";
+import "./style.scss";
 
 export default function Login(props) {
-    return (
-        <div>
-                <Input
-                    value={props.email}
-                    onChange={props.handleInputChange}
-                    name="email"
-                    placeholder="Email"
-                />
-                <Input
-                    type="password"
-                    value={props.password}
-                    onChange={props.handleInputChange}
-                    name="password"
-                    placeholder="Password"
-                />
-                <button style={{ float: "right", marginBottom: 10 }} className="btn btn-success" onClick={() => props.loginOnClick(props.email, props.password)}>
-                    Log In
-                </button>
-        </div>
-            
-    )
+  return (
+    <div className="text-center login">
+    <h2>Log In</h2>
+      <input className="col-md-10"
+        value={props.email}
+        onChange={props.handleInputChange}
+        name="email"
+        placeholder="Email"
+      />
+      <input className="col-md-10"
+        type="password"
+        value={props.password}
+        onChange={props.handleInputChange}
+        name="password"
+        placeholder="Password"
+      />
+      <button
+        //style={{ float: "right", marginBottom: 10 }}
+        className="btn btn-success"
+        onClick={() => props.loginOnClick(props.email, props.password)}
+      >
+        Log In
+      </button>
+    </div>
+  );
 }
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,99 +1,113 @@
 import React, { Component } from "react";
-import { Redirect } from 'react-router-dom';
-import { Container, Row } from "../../components/Grid/index";
-import Login from "../../components/Login/Login"
+import { Redirect } from "react-router-dom";
+//import { Container, Row } from "../../components/Grid/index";
+//import Login from "../../components/Login/Login";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import API from "../../utils/API";
-import axios from "axios"
+import axios from "axios";
 
 class Signup extends Component {
+  state = {
+    userType: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    subject: "",
+    subjects: [],
+    success: false
+  };
 
-    state = {
-        userType: "",
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        subject: "",
-        subjects: [],
-        success: false
-    }
-
-    signUpOnClick = (userType, firstName, lastName, email, password, subjects) => {
-        API.createUser({
-            userType: userType,
-            firstName: firstName,
-            lastName: lastName,
-            subjects: subjects,
-            email: email,
-            password: password,
-        })
-            .then(res => {
-                this.setState({
-                    userType: "",
-                    firstName: "",
-                    lastName: "",
-                    email: "",
-                    password: "",
-                    subject: "",
-                    subjects: []
-                })
-            })
-            .catch(err => console.log(err));
-    }
-
-
-    loginOnClick = () => {
-        const { email, password } = this.state
-        axios.post("/auth/login", { email, password })
-            .then((res) => {
-                console.log(res.data)
-                localStorage.setItem("jwtToken", res.data.token);
-                this.setState({
-                    success: res.data.success
-                });
-            })
-            .catch(err => console.log(err))
-    }
-
-    pushSubject = (subject) => {
-        this.setState({ subjects: [...this.state.subjects, subject] });
-        this.setState({ subject: "" })
-    };
-
-    handleInputChange = event => {
-        const { name, value } = event.target;
+  signUpOnClick = (
+    userType,
+    firstName,
+    lastName,
+    email,
+    password,
+    subjects
+  ) => {
+    API.createUser({
+      userType: userType,
+      firstName: firstName,
+      lastName: lastName,
+      subjects: subjects,
+      email: email,
+      password: password
+    })
+      .then(res => {
         this.setState({
-            [name]: value
+          userType: "",
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          subject: "",
+          subjects: []
         });
-    };
+      })
+      .catch(err => console.log(err));
+  };
 
-    render() {
-        if (this.state.success) {
-            return (<Redirect to="/" />)
-        } else {
-            return (
-                <Container>
-                    <Row>
-                
-                    
-                        <SignUpForm
-                            userType={this.state.userType}
-                            firstName={this.state.firstName}
-                            lastName={this.state.lastName}
-                            email={this.state.email}
-                            password={this.state.password}
-                            subject={this.state.subject}
-                            subjects={this.state.subjects}
-                            handleInputChange={this.handleInputChange}
-                            signUpOnClick={this.signUpOnClick}
-                            pushSubject={this.pushSubject}
-                        />
-                    </Row>
-                </Container>
-            )
-        }
+  loginOnClick = () => {
+    const { email, password } = this.state;
+    axios
+      .post("/auth/login", { email, password })
+      .then(res => {
+        console.log(res.data);
+        localStorage.setItem("jwtToken", res.data.token);
+        this.setState({
+          success: res.data.success
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  pushSubject = subject => {
+    this.setState({ subjects: [...this.state.subjects, subject] });
+    this.setState({ subject: "" });
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    if (this.state.success) {
+      return <Redirect to="/" />;
+    } else {
+      return (
+        <div className="container">
+          <div className="row">
+            {/* <div className="col-md-4 float-left">
+              <Login
+                email={this.state.email}
+                password={this.state.password}
+                handleInputChange={this.handleInputChange}
+                loginOnClick={this.loginOnClick}
+              />
+            </div> */}
+            <div className="col-md-8 mx-auto">
+              <SignUpForm
+                userType={this.state.userType}
+                firstName={this.state.firstName}
+                lastName={this.state.lastName}
+                email={this.state.email}
+                password={this.state.password}
+                subject={this.state.subject}
+                subjects={this.state.subjects}
+                handleInputChange={this.handleInputChange}
+                signUpOnClick={this.signUpOnClick}
+                pushSubject={this.pushSubject}
+              />
+            </div>
+          </div>
+        </div>
+      );
     }
+  }
 }
 
 export default Signup;
