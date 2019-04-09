d71c39e02574897a92941e892bd92817d16af443
Scripted Well <whigareda@theblackpilledpage.com>
2019-04-08 21:01:35 -0700

    styling started

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+//import { Redirect } from "react-router-dom";
 import { Container, Row } from "../../components/Grid/index";
 //import Login from "../../components/Login/Login";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
@@ -19,87 +19,64 @@ class Signup extends Component {
   };
 
 
-    state = {
-        userType: "",
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        subject: "",
-        subjects: [],
-        success: false
-    }
-
-    signUpOnClick = (userType, firstName, lastName, email, password, subjects) => {
-        API.createUser({
-            userType: userType,
-            firstName: firstName,
-            lastName: lastName,
-            subjects: subjects,
-            email: email,
-            password: password,
-        })
-            .then(res => {
-                this.setState({
-                    userType: "",
-                    firstName: "",
-                    lastName: "",
-                    email: "",
-                    password: "",
-                    subject: "",
-                    subjects: []
-                })
-            })
-            .catch(err => console.log(err));
-    }
-
-    pushSubject = (subject) => {
-        this.setState({ subjects: [...this.state.subjects, subject] });
-        this.setState({ subject: "" })
-    };
-
-
-  loginOnClick = () => {
-    const { email, password } = this.state;
-    axios
-      .post("/auth/login", { email, password })
+  signUpOnClick = (userType, firstName, lastName, email, password, subjects) => {
+    API.createUser({
+      userType: userType,
+      firstName: firstName,
+      lastName: lastName,
+      subjects: subjects,
+      email: email,
+      password: password,
+    })
       .then(res => {
-        console.log(res.data);
-        localStorage.setItem("jwtToken", res.data.token);
         this.setState({
-          success: res.data.success
-        });
+          userType: "",
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          subject: "",
+          subjects: []
+        })
       })
       .catch(err => console.log(err));
-  };
+  }
 
-  pushSubject = subject => {
+  pushSubject = (subject) => {
     this.setState({ subjects: [...this.state.subjects, subject] });
-    this.setState({ subject: "" });
+    this.setState({ subject: "" })
   };
 
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <SignUpForm
-                        userType={this.state.userType}
-                        firstName={this.state.firstName}
-                        lastName={this.state.lastName}
-                        email={this.state.email}
-                        password={this.state.password}
-                        subject={this.state.subject}
-                        subjects={this.state.subjects}
-                        handleInputChange={this.handleInputChange}
-                        signUpOnClick={this.signUpOnClick}
-                        pushSubject={this.pushSubject}
-                    />
-                </Row>
-            </Container>
-        )
 
-    }
-  
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <SignUpForm
+            userType={this.state.userType}
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            email={this.state.email}
+            password={this.state.password}
+            subject={this.state.subject}
+            subjects={this.state.subjects}
+            handleInputChange={this.handleInputChange}
+            signUpOnClick={this.signUpOnClick}
+            pushSubject={this.pushSubject}
+          />
+        </Row>
+      </Container>
+    )
+
+  }
+
 }
 
 export default Signup;
