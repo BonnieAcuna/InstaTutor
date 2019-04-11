2a0bc6a56ccacbe04330d5cd9959afd9925ef5df
Bonnie <bonnieacuna@gmail.com>
2019-04-10 18:10:04 -0700

    updates

--- client/src/components/FeaturedTutors/FeaturedTutors.js
+++ client/src/components/FeaturedTutors/FeaturedTutors.js
@@ -1,34 +1,38 @@
 import React, { Component } from "react";
 import "./style.scss";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Row, Col } from "../../components/Grid/index";
 
 class FeaturedTutors extends Component {
 
 
     render(props) {
-        
+
         return (
             <Row>
-            <Col size="sm-2">
-            </Col>
-            <Col size="sm-8">
-            <Link to={`/user/${this.props.userid}`}>
-                <div className="container">
-                    <div className="card shadow-sm bg-white rounded">
-                        <div className="card-img-top">
-                            <img src={this.props.image} alt="Random Tutor" />
-                            <div className="card-body">
-                                <p className="card-text">{this.props.name}<br></br>
-                                {this.props.subjects}</p>
+                <Col size="sm-2">
+                </Col>
+                <Col size="sm-8">
+                    <Link to={`/user/${this.props.userid}`}>
+
+                        <div className="card">
+                            <div className="card-image">
+                                <img src={this.props.image} alt="Random Tutor" />{this.props.name}
+                                    {/* <div className="card-title">
+                                        {this.props.name}
+                                    </div> */}
                             </div>
+                                <div className="card-footer">
+                                    <div className="card-description">
+                                        {this.props.subjects}
+                                    </div>
+                                </div>
                         </div>
-                    </div>
-                </div>
-            </Link>
-            </Col>
-            <Col size="sm-2">
-            </Col>
+
+                    </Link>
+                </Col>
+                <Col size="sm-2">
+                </Col>
             </Row>
         )
     }
